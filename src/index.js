40fe8dddb474b68e73f768300799e146be6aefea
Rehman Javaid <rehmanjav@outlook.com>
2022-12-10 17:17:24 -0500

    Setup classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+class Project {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Note {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+    }
+}
+
+function renderIndex() {
+    let body = document.querySelector('body');
+    
+    body.innerHTML = `
+    
+    `;
+}
